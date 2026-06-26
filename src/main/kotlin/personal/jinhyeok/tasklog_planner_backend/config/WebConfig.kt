6e776351323048d7347070ceb54b98ed7b2824fb
jhheo <hwlsgur1120@gmail.com>
2026-06-26 15:53:16 +0900

    feat: origin url to env variable

--- src/main/kotlin/personal/jinhyeok/tasklog_planner_backend/config/WebConfig.kt
+++ src/main/kotlin/personal/jinhyeok/tasklog_planner_backend/config/WebConfig.kt
@@ -1,18 +1,18 @@
 package personal.jinhyeok.tasklog_planner_backend.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    @Value($$"${app.cors.allowed-origins}") private val allowedOrigins: String,
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOriginPatterns(
-                "http://tasklog-planner.local",
-                "http://tasklog-planner.local:*",
-                "https://tasklog-planner.local",
-                "https://tasklog-planner.local:*",
+                *allowedOrigins.split(",").map { it.trim() }.toTypedArray()
             )
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
